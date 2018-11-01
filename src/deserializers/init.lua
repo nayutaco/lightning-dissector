993ce1e11cc3b68966fdc68dcfc48d14efdaf15d
tock203 <tick@tock.tech>
2018-11-01 14:09:54 +0900

    Use ProtoField in init deserializer

--- src/deserializers/init.lua
+++ src/deserializers/init.lua
@@ -3,6 +3,7 @@ local inspect = require "inspect"
 local Reader = require("lightning-dissector.utils").Reader
 local OrderedDict = require("lightning-dissector.utils").OrderedDict
 local deserialize_flags = require("lightning-dissector.utils").deserialize_flags
+local fields = require("lightning-dissector.constants").fields
 
 function deserialize(payload)
   local reader = Reader:new(payload)
@@ -16,24 +17,24 @@ function deserialize(payload)
 
   return OrderedDict:new(
     "gflen", OrderedDict:new(
-      "Raw", bin.stohex(packed_gflen),
-      "Deserialized", gflen
+      fields.payload.deserialized.gflen.raw, bin.stohex(packed_gflen),
+      fields.payload.deserialized.gflen.deserialized, gflen
     ),
-    "global_features", bin.stohex(packed_global_features),
+    fields.payload.deserialized.global_features, bin.stohex(packed_global_features),
     "lflen", OrderedDict:new(
-      "Raw", bin.stohex(packed_lflen),
-      "Deserialized", lflen
+      fields.payload.deserialized.lflen.raw, bin.stohex(packed_lflen),
+      fields.payload.deserialized.lflen.deserialized, lflen
     ),
     "local_features", OrderedDict:new(
-      "Raw", bin.stohex(packed_local_features),
+      fields.payload.deserialized.local_features.raw, bin.stohex(packed_local_features),
       "Deserialized", OrderedDict:new(
-        "Optional", inspect(deserialize_flags(packed_local_features, {
+        fields.payload.deserialized.local_features.deserialized.optional, inspect(deserialize_flags(packed_local_features, {
           [2] = "option_data_loss_protect",
           [4] = "initial_routing_sync",
           [6] = "option_upfront_shutdown_script",
           [8] = "gossip_queries"
         })),
-        "Required", inspect(deserialize_flags(packed_local_features, {
+        fields.payload.deserialized.local_features.deserialized.required, inspect(deserialize_flags(packed_local_features, {
           [1] = "option_data_loss_protect",
           [5] = "option_upfront_shutdown_script",
           [7] = "gossip_queries",
