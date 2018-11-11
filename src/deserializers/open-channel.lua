f1f32e5d3ab15f612195b0b14e14519ad8d644dc
tock203 <tick@tock.tech>
2018-11-12 03:16:27 +0900

    Use ProtoFiled in open_channel deserializer

--- src/deserializers/open-channel.lua
+++ src/deserializers/open-channel.lua
@@ -3,6 +3,7 @@ local inspect = require "inspect"
 local Reader = require("lightning-dissector.utils").Reader
 local OrderedDict = require("lightning-dissector.utils").OrderedDict
 local deserialize_flags = require("lightning-dissector.utils").deserialize_flags
+local f = require("lightning-dissector.constants").fields.payload.deserialized
 
 function deserialize(payload)
   local reader = Reader:new(payload)
@@ -27,53 +28,53 @@ function deserialize(payload)
   local packed_channel_flags = reader:read(1)
 
   local result = OrderedDict:new(
-    "chain_hash", bin.stohex(packed_chain_hash),
-    "temporary_channel_id", bin.stohex(packed_temporary_channel_id),
+    f.chain_hash, bin.stohex(packed_chain_hash),
+    f.temporary_channel_id, bin.stohex(packed_temporary_channel_id),
     "funding_satoshis", OrderedDict:new(
-      "Raw", bin.stohex(packed_funding_satoshis),
-      "Deserialized", (string.unpack(">I8", packed_funding_satoshis))
+      f.funding_satoshis.raw, bin.stohex(packed_funding_satoshis),
+      f.funding_satoshis.deserialized, UInt64.decode(packed_funding_satoshis, false)
     ),
     "push_msat", OrderedDict:new(
-      "Raw", bin.stohex(packed_push_msat),
-      "Deserialized", (string.unpack(">I8", packed_push_msat))
+      f.push_msat.raw, bin.stohex(packed_push_msat),
+      f.push_msat.deserialized, UInt64.decode(packed_push_msat, false)
     ),
     "dust_limit_satoshis", OrderedDict:new(
-      "Raw", bin.stohex(packed_dust_limit_satoshis),
-      "Deserialized", (string.unpack(">I8", packed_dust_limit_satoshis))
+      f.dust_limit_satoshis.raw, bin.stohex(packed_dust_limit_satoshis),
+      f.dust_limit_satoshis.deserialized, UInt64.decode(packed_dust_limit_satoshis, false)
     ),
     "max_htlc_value_in_flight_msat", OrderedDict:new(
-      "Raw", bin.stohex(packed_max_htlc_value_in_flight_msat),
-      "Deserialized", (string.unpack(">I8", packed_max_htlc_value_in_flight_msat))
+      f.max_htlc_value_in_flight_msat.raw, bin.stohex(packed_max_htlc_value_in_flight_msat),
+      f.max_htlc_value_in_flight_msat.deserialized, UInt64.decode(packed_max_htlc_value_in_flight_msat, false)
     ),
     "channel_reserve_satoshis", OrderedDict:new(
-      "Raw", bin.stohex(packed_channel_reserve_satoshis),
-      "Deserialized", (string.unpack(">I8", packed_channel_reserve_satoshis))
+      f.channel_reserve_satoshis.raw, bin.stohex(packed_channel_reserve_satoshis),
+      f.channel_reserve_satoshis.deserialized, UInt64.decode(packed_channel_reserve_satoshis, false)
     ),
     "htlc_minimum_msat", OrderedDict:new(
-      "Raw", bin.stohex(packed_htlc_minimum_msat),
-      "Deserialized", (string.unpack(">I8", packed_htlc_minimum_msat))
+      f.htlc_minimum_msat.raw, bin.stohex(packed_htlc_minimum_msat),
+      f.htlc_minimum_msat.deserialized, UInt64.decode(packed_htlc_minimum_msat, false)
     ),
     "feerate_per_kw", OrderedDict:new(
-      "Raw", bin.stohex(packed_feerate_per_kw),
-      "Deserialized", (string.unpack(">I4", packed_feerate_per_kw))
+      f.feerate_per_kw.raw, bin.stohex(packed_feerate_per_kw),
+      f.feerate_per_kw.deserialized, (string.unpack(">I4", packed_feerate_per_kw))
     ),
     "to_self_delay", OrderedDict:new(
-      "Raw", bin.stohex(packed_to_self_delay),
-      "Deserialized", (string.unpack(">I2", packed_to_self_delay))
+      f.to_self_delay.raw, bin.stohex(packed_to_self_delay),
+      f.to_self_delay.deserialized, (string.unpack(">I2", packed_to_self_delay))
     ),
     "max_accepted_htlcs", OrderedDict:new(
-      "Raw", bin.stohex(packed_max_accepted_htlcs),
-      "Deserialized", (string.unpack(">I2", packed_max_accepted_htlcs))
+      f.max_accepted_htlcs.raw, bin.stohex(packed_max_accepted_htlcs),
+      f.max_accepted_htlcs.deserialized, (string.unpack(">I2", packed_max_accepted_htlcs))
     ),
-    "funding_pubkey", bin.stohex(packed_funding_pubkey),
-    "revocation_basepoint", bin.stohex(packed_revocation_basepoint),
-    "payment_basepoint", bin.stohex(packed_payment_basepoint),
-    "delayed_payment_basepoint", bin.stohex(packed_delayed_payment_basepoint),
-    "htlc_basepoint", bin.stohex(packed_htlc_basepoint),
-    "first_per_commitment_point", bin.stohex(packed_first_per_commitment_point),
+    f.funding_pubkey, bin.stohex(packed_funding_pubkey),
+    f.revocation_basepoint, bin.stohex(packed_revocation_basepoint),
+    f.payment_basepoint, bin.stohex(packed_payment_basepoint),
+    f.delayed_payment_basepoint, bin.stohex(packed_delayed_payment_basepoint),
+    f.htlc_basepoint, bin.stohex(packed_htlc_basepoint),
+    f.first_per_commitment_point, bin.stohex(packed_first_per_commitment_point),
     "channel_flags", OrderedDict:new(
-      "Raw", bin.stohex(packed_channel_flags),
-      "Deserialized", inspect(deserialize_flags(packed_channel_flags, {"announce_channel"}))
+      f.channel_flags.raw, bin.stohex(packed_channel_flags),
+      f.channel_flags.deserialized, inspect(deserialize_flags(packed_channel_flags, {"announce_channel"}))
     )
   )
 
@@ -83,10 +84,10 @@ function deserialize(payload)
     local packed_shutdown_scriptpubkey = reader:read(shutdown_len)
 
     result:append("shutdown_len", OrderedDict:new(
-      "Raw", bin.stohex(packed_shutdown_len),
-      "Deserialized", shutdown_len
+      f.shutdown_len.raw, bin.stohex(packed_shutdown_len),
+      f.shutdown_len.deserialized, shutdown_len
     ))
-    result:append("shutdown_scriptpubkey", bin.stohex(packed_shutdown_scriptpubkey))
+    result:append(f.shutdown_scriptpubkey, bin.stohex(packed_shutdown_scriptpubkey))
   end
 
   return result
