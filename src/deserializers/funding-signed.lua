d0d15b06785df1f5d4440cc6470c267e3f5b441c
tock203 <tick@tock.tech>
2018-11-12 06:01:42 +0900

    Use ProtoFiled in funding_signed deserializer

--- src/deserializers/funding-signed.lua
+++ src/deserializers/funding-signed.lua
@@ -2,6 +2,7 @@ local bin = require "plc52.bin"
 local Reader = require("lightning-dissector.utils").Reader
 local OrderedDict = require("lightning-dissector.utils").OrderedDict
 local convert_signature_der = require("lightning-dissector.utils").convert_signature_der
+local f = require("lightning-dissector.constants").fields.payload.deserialized
 
 function deserialize(payload)
   local reader = Reader:new(payload)
@@ -10,10 +11,10 @@ function deserialize(payload)
   local packed_signature = reader:read(64)
 
   return OrderedDict:new(
-    "channel_id", bin.stohex(packed_channel_id),
+    f.channel_id, bin.stohex(packed_channel_id),
     "signature", OrderedDict:new(
-      "Raw", bin.stohex(packed_signature),
-      "DER", bin.stohex(convert_signature_der(packed_signature))
+      f.signature.raw, bin.stohex(packed_signature),
+      f.signature.der, bin.stohex(convert_signature_der(packed_signature))
     )
   )
 end
